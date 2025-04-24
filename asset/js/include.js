3cf3f824ee6d2767677de15a9ff382cb2fbc0747
ahmed tliche <tlicheahmed76100@gmail.com>
2025-04-24 13:24:03 +0200

    v0.4 modification de blog footer nav

--- asset/js/include.js
+++ asset/js/include.js
@@ -18,3 +18,14 @@ function includeHTML() {
 }
 
 document.addEventListener("DOMContentLoaded", includeHTML);
+
+
+const btn = document.getElementById('mobile-menu-button');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+    const isExpanded = btn.getAttribute('aria-expanded') === 'true';
+
+    btn.setAttribute('aria-expanded', String(!isExpanded));
+    menu.classList.toggle('hidden');
+});
